5594603b4fcefc05fee516b54644e9471c58c950
Egor Onekeev <sergeev_e_k@mail.ru>
2024-10-14 20:04:12 +0300

    add test

--- tests/service_test.go
+++ tests/service_test.go
@@ -1,26 +1,37 @@
 package tests
 
 import (
+	"fmt"
 	"music/internal/models"
 	"music/internal/service"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type TestStorage struct {
+type TestService struct {
 	service.StorageInt
 }
 
-func (s *TestStorage) AddSong(song models.AddSong) (string, error) {
-	return "testSong added to testGroup", nil
+func (s *TestService) AddSong(song models.AddSong) (string, error) {
+	return "ADD", nil
+}
+
+func (s *TestService) Info(groupName string, songTitle string) (models.SongDetail, error) {
+	return models.SongDetail{
+		ReleaseDate: "TEST",
+		Text:        "TEST",
+		Link:        "TEST",
+	}, nil
+
 }
 
 func TestAddSong(t *testing.T) {
 
 	// Создаем тестовую структуру
-	testStorage := &TestStorage{}
-	svc := service.New(testStorage)
+	testService := &TestService{}
+	svc := service.New(testService)
 
 	// 1. Тест Happy
 	testSong := models.AddSong{
@@ -32,8 +43,8 @@ func TestAddSong(t *testing.T) {
 	result, err := svc.AddSong(testSong)
 
 	// Проверяем, что метод вернул ожидаемые значения
-	if result != "testSong added to testGroup" {
-		t.Errorf("expected 'testSong added to testGroup', got %s", result)
+	if result != "ADD" {
+		t.Errorf("expected 'ADD', got %s", result)
 	}
 	assert.NoError(t, err)
 
@@ -54,6 +65,48 @@ func TestAddSong(t *testing.T) {
 		GroupName: "SongTest",
 	}
 	_, err = svc.AddSong(emptySong)
+	assert.Error(t, err)
+}
+
+func TestInfo(t *testing.T) {
+
+	testServ := &TestService{}
+	srv := service.New(testServ)
+
+	// 1. Тест Happy
+	fullSong := models.Song{
+		Title: "TEST",
+		Group: "TEST",
+	}
+
+	answ, err := srv.Info(fullSong)
+	if err != nil {
+		t.Errorf("expected nil error, got %s", err)
+	}
+	expectedAnsw := fmt.Sprintf("Дата релиза: %s, Текст: %s, Ссылка: %s", "TEST", "TEST", "TEST")
+
+	if !reflect.DeepEqual(answ, expectedAnsw) {
+		t.Errorf("expected %v, got %v", expectedAnsw, answ)
+	}
+	assert.NoError(t, err)
+
+	// 2. Тест emptyReq
+	emptyReq := models.Song{}
+	_, err = srv.Info(emptyReq)
+	assert.Error(t, err)
+
+	// 3. Тест emptyGroup
+	emptyGroup := models.Song{
+		Title: "TEST",
+	}
+	_, err = srv.Info(emptyGroup)
+	assert.Error(t, err)
+
+	// 4. Тест emptySong
+	emptySong := models.Song{
+		Group: "TEST",
+	}
+	_, err = srv.Info(emptySong)
 	assert.Error(t, err)
 
 }
